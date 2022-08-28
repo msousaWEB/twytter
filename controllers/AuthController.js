3286a35c50fde56e16e9cc20da6a49c02388f2a2
msousa <msousa.dev@outlook.com>
2022-08-28 11:21:42 -0300

    implementing flash messages

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login')
@@ -6,4 +9,17 @@ module.exports = class AuthController {
     static register(req, res) {
         res.render('auth/register')
     };
+
+    static async registerPost(req,res) {
+        const { name, email, password, confirmpassword } = req.body
+
+        //confirmacao de senha
+        if(password != confirmpassword) {
+            //mensagem de erro
+            req.flash('error', 'Confirmação de senha incorreta!')
+            res.render('auth/register')
+
+            return
+        }
+    }
 }
