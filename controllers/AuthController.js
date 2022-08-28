6ac8a3fa7aa1439e281b53b0d2e9f8a916130394
msousa <msousa.dev@outlook.com>
2022-08-28 11:45:20 -0300

    registering users in the system

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -21,5 +21,42 @@ module.exports = class AuthController {
 
             return
         }
+
+        //checar se usuário já existe
+        const checkExistUser = await User.findOne({
+            where: {email: email}
+        })
+        if(checkExistUser) {
+            req.flash('error', 'Já existe uma conta com este e-mail!')
+            res.render('auth/register')
+
+            return
+        }
+
+        //criar senha
+        const salt = bcrypt.genSaltSync(10)
+        const hashPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashPassword
+        }
+
+        try {
+           const createdUser = await User.create(user)
+
+            // inicializar a sessao
+            req.session.userid = createdUser.id
+
+            req.flash('success', 'Cadastro realizado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/')
+            })
+
+        } catch(err) {
+            console.log(err)
+        }
     }
 }
